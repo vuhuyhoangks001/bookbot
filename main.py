1a1ac1e7b38cfe34aa3bed7894631756313823fd
vuhuyhoangks001 <vuhuyhoangks001@gmail.com>
2023-02-16 16:45:33 +0700

    Update main.py print a report

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def main():
     text = get_book_text("books/frankenstein.txt")
     number_of_words = count_words(text)
-    print(number_of_words)
-
     number_of_letters = count_letters(text)
-    print(number_of_letters)
+    report = report_begin.format(number_of_words) + count_dict_report(number_of_letters) + report_end
+    print(report)
 
 def count_words(text):
     sum = 0
@@ -13,7 +12,6 @@ def count_words(text):
         sum += 1
 
     return sum
-
 
 def get_book_text(path):
     with open(path) as f:
@@ -30,5 +28,25 @@ def count_letters(text):
 
     return letter_dict
 
+report_begin = """
+--- Begin report of books/frankenstein.txt ---
+{0} words found in the document
+
+"""
+
+report_end = """
+--- End report ---
+"""
+
+def count_dict_report(dict):
+    result = ""
+    sorted_dict_list = sorted(dict.items(), key= lambda x:x[1], reverse=True)
+
+    for tupl in sorted_dict_list:
+        if tupl[0].isalpha():
+            line_report = f"The '{tupl[0]}' character was found {tupl[1]} times"
+            result += line_report + "\n"
+
+    return result
 
 main()
